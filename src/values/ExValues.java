17be88420770d6fccdffaa43d5fc569163c29ece
ashfaqgithub <huque.md.ashfaqul@gmail.com>
2019-05-22 22:38:52 -0400

    s

---
+++ src/values/ExValues.java
@@ -0,0 +1,33 @@
+package values;
+
+public class ExValues {
+
+	public String firstNameV = "Kuddus";
+	
+	public String lastNameV = "Ali";
+	
+	public String email = "seleniumtest2019@gmail.com";
+	
+	public String pass = "gotonaki";
+	
+	
+//  Car Booking
+	
+	public String pickingUp= "New York Jfk";
+	public String droppingOff= "New York JFK";
+	public String pickUpDate="5/30/2019";
+	public String dropOffDate="6/1/2019";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
